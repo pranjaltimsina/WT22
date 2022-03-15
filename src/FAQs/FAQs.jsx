d5868fd011173640c765d726d2a92ebe093eaf28
Pranjal Timsina <pranjal.timsina@live.com>
2022-03-15 22:34:47 +0545

    feat: changed faq for large screens

--- src/FAQs/FAQs.jsx
+++ src/FAQs/FAQs.jsx
@@ -2,9 +2,9 @@ import FAQ from "./FAQ"
 
 const FAQs = ({ myRef }) => {
   return (
-    <section ref={myRef} className="w-full h-max md:h-screen bg-[#5935a4] py-10 md:px-16 overflow-hidden">
-      <h3 className='w-full text-center sm:text-left text-[#ff7f4d] font-DelaGothic text-5xl md:text-7xl px-10 md:px-0'>FAQ'S</h3>
-      <div className='flex flex-col md:flex-row md:flex-wrap md:justify-between items-center py-10 md:px-0'>
+    <section ref={myRef} className="w-full h-max  bg-[#5935a4] py-10 md:px-16 md:py-28 overflow-hidden">
+      <h3 className='w-full text-center  text-[#ff7f4d] font-DelaGothic text-5xl md:text-7xl px-10 md:px-0'>FAQ'S</h3>
+      <div className='flex flex-col items-center py-10 md:px-0'>
         <FAQ question='This is a demo FAQ?' answer='No, one does not need to be online for the entire duration. You can develop the application on your local system based on the given themes and then submit it on HackerEarth, on the specific challenge page.'/>
         <FAQ question='This is a demo FAQ?' answer='No, one does not need to be online for the entire duration. You can develop the application on your local system based on the given themes and then submit it on HackerEarth, on the specific challenge page.'/>
         <FAQ question='This is a demo FAQ?' answer='No, one does not need to be online for the entire duration. You can develop the application on your local system based on the given themes and then submit it on HackerEarth, on the specific challenge page.'/>
